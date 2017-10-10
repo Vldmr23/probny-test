685205c9a2d42b96c9da5c62192c98b9b655258d
Владимир <v.makashin@yandex.ru>
2017-10-10 15:43:56 +0300

    I created new java project

---
+++ Git-test-project/src/by/zti/test/Main.java
@@ -0,0 +1,11 @@
+package by.zti.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello from Git Hub");
+	}
+
+}
